764532cafe427dc232a92a93ed87efb7ed4cfe14
slashexx <dhruvpuri.35@gmail.com>
2025-12-08 17:30:59 +0530

    change header to OAAS
    Signed-off-by: slashexx <dhruvpuri.35@gmail.com>

--- src/conf.h
+++ src/conf.h
@@ -507,7 +507,7 @@ using upx::tribool;
 #define EXIT_INTERNAL   1
 
 // magic constants for patching
-#define UPX_MAGIC_LE32  0x21585055 /* "UPX!" */
+#define UPX_MAGIC_LE32  0x5341414f /* "OAAS" custom magic */
 #define UPX_MAGIC2_LE32 0xD5D0D8A1
 
 // upx_compress() error codes
--- src/stub/src/include/darwin.h
+++ src/stub/src/include/darwin.h
@@ -104,7 +104,7 @@ extern ssize_t write(int, char const *, size_t);
 // UPX stuff
 **************************************************************************/
 
-#define UPX_MAGIC_LE32  0x21585055          // "UPX!"
+#define UPX_MAGIC_LE32  0x5341414f          // "OAAS"
 
 
 #define nrv_byte unsigned char
